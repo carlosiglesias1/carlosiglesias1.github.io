b8951b31062b423cce0febfe3c2a27fd4b1a3b5e
Carlos <carlosig490@gmail.com>
2024-10-12 17:18:02 +0200

    migrating angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
   { path: '**', component: PresentationComponent },
   { path: 'welcome', component: PresentationComponent }
 ];
