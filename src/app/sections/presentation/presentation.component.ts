bf9266cf1b0ed1031a3eeaa131ff2095a68ba993
Carlos <carlosig490@gmail.com>
2026-01-11 21:12:24 +0100

    reedición de la página de presentación

--- src/app/sections/presentation/presentation.component.ts
+++ src/app/sections/presentation/presentation.component.ts
@@ -3,7 +3,8 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { ImagesService } from '../../services/images.service';
 import { NotificationService } from 'src/app/services/notification.service';
-import { svglCsharp } from '@ng-icons/svgl'
+import { svglCsharp, svglSqlServer } from '@ng-icons/svgl'
+import { bootstrapGit } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-presentation',
@@ -26,7 +27,7 @@ export class PresentationComponent implements OnInit {
 
   protected currentlyWorkingOnChart: {
     data: {
-      datasets: Array<{ data: number[] }>,
+      datasets: Array<{ backgroundColor: string[], data: number[] }>,
       labels: string[]
     },
     options: {}
@@ -74,6 +75,46 @@ export class PresentationComponent implements OnInit {
             </defs>
           </svg>`
 
+  protected dotnetIcon: string = `<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" fill-rule="evenodd" clip-rule="evenodd"
+          stroke-linejoin="round" stroke-miterlimit="2">
+          <path fill="#512bd4" d="M-.134-.326h512.002v512.002H-.134z" />
+          <path
+            d="M91.122 326.786c-3.62 0-6.698-1.206-9.232-3.619-2.534-2.475-3.8-5.413-3.8-8.815 0-3.465 1.266-6.434 3.8-8.908 2.534-2.475 5.612-3.712 9.232-3.712 3.68 0 6.787 1.237 9.321 3.712 2.595 2.474 3.892 5.443 3.892 8.908 0 3.402-1.297 6.34-3.892 8.815-2.534 2.413-5.64 3.619-9.321 3.619zM235.844 324.745h-23.532l-61.996-97.807a43.764 43.764 0 01-3.892-7.703h-.543c.483 2.847.724 8.94.724 18.28v87.23h-20.817v-133.07h25.07l59.916 95.487c2.534 3.96 4.163 6.682 4.887 8.166h.362c-.603-3.525-.905-9.495-.905-17.91v-85.743h20.726v133.07zM337.213 324.745h-72.856v-133.07h69.96v18.745h-48.42v37.675h44.62v18.652h-44.62v39.346h51.316v18.652zM440.757 210.42h-37.289v114.325h-21.54V210.42H344.73v-18.745h96.027v18.745z"
+            fill="#fff" fill-rule="nonzero" />
+        </svg>`
+
+  protected devOpsIcon: string = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 18 18">
+        <defs>
+          <linearGradient id="ba420277-700e-42cc-9de9-5388a5c16e54" x1="9" y1="16.97" x2="9" y2="1.03"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#0078d4" />
+            <stop offset="0.16" stop-color="#1380da" />
+            <stop offset="0.53" stop-color="#3c91e5" />
+            <stop offset="0.82" stop-color="#559cec" />
+            <stop offset="1" stop-color="#5ea0ef" />
+          </linearGradient>
+        </defs>
+        <title>Icon-devops-261</title>
+        <path id="a91f0ca4-8fb7-4019-9c09-0a52e2c05754"
+          d="M17,4v9.74l-4,3.28-6.2-2.26V17L3.29,12.41l10.23.8V4.44Zm-3.41.49L7.85,1V3.29L2.58,4.84,1,6.87v4.61l2.26,1V6.57Z"
+          fill="url(#ba420277-700e-42cc-9de9-5388a5c16e54)" />
+      </svg>`
+
+  protected gmailIcon: string = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 49.4 512 399.42" height="100%"  width="100%">
+            <g fill="none" fill-rule="evenodd">
+              <g fill-rule="nonzero">
+                <path fill="#4285f4"
+                  d="M34.91 448.818h81.454V251L0 163.727V413.91c0 19.287 15.622 34.91 34.91 34.91z" />
+                <path fill="#34a853"
+                  d="M395.636 448.818h81.455c19.287 0 34.909-15.622 34.909-34.909V163.727L395.636 251z" />
+                <path fill="#fbbc04" d="M395.636 99.727V251L512 163.727v-46.545c0-43.142-49.25-67.782-83.782-41.891z" />
+              </g>
+              <path fill="#ea4335" d="M116.364 251V99.727L256 204.455 395.636 99.727V251L256 355.727z" />
+              <path fill="#c5221f" fill-rule="nonzero"
+                d="M0 117.182v46.545L116.364 251V99.727L83.782 75.291C49.25 49.4 0 74.04 0 117.18z" />
+            </g>
+          </svg>`
+
   protected siemensIcon: string = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 64 64">
             <path d="M0 0h64v64H0z" fill="#099" />
             <path
@@ -95,7 +136,25 @@ export class PresentationComponent implements OnInit {
     }
 
     this.currentlyWorkingOnChart = {
-      data: { datasets: [{ data: [55, 85, 70, 75, 85] }], labels: ['SimaticIT', 'AngularJS', 'Angular', '.Net/.Net Framework', 'Opcenter'] },
+      data: {
+        datasets: [
+          {
+            backgroundColor: [
+              '#50BED795',
+              '#41AAAA95',
+              '#b52e3195',
+              '#C3002F95',
+              '#F1502F95',
+              '#605ca995',
+              '#de3d8195',
+              '#d3d3d39f',
+              '#0080FF95'
+            ],
+            data: [45, 85, 85, 55, 66, 75, 70, 60, 37]
+          }
+        ],
+        labels: ['SimaticIT', 'Opcenter', 'AngularJS', 'Angular', 'Git', '.Net/.Net Framework', 'Entity Framework', 'SQL Server', 'Azure DevOps']
+      },
       options: {
         borderWidth: 1,
         layout: {
@@ -186,13 +245,24 @@ export class PresentationComponent implements OnInit {
                   td.style.borderWidth = '0'
                   td.innerHTML = `<div style="height: 70px">${(() => {
                     switch (title) {
+                      case '.Net/.Net Framework': return this.dotnetIcon
                       case 'Angular':
                       case 'AngularJS': return this.angularIcon
-                      case '.Net/.Net Framework':
-                        let svgParts = svglCsharp.split('svg ')
-                        return `${svgParts[0]}svg height="100%" width="100%" ${svgParts[1]}`
+                      case 'Azure DevOps': return this.devOpsIcon
                       case 'Opcenter':
                       case 'SimaticIT': return this.siemensIcon
+                      case 'Git':{
+                        const [_, git] = bootstrapGit.split('svg ')
+                        return `<svg height="100%" width="100%" style="color: #F1502F" ${git}`
+                      }
+                      case 'SQL Server': {
+                        const [_, sqlServer] = svglSqlServer.split('svg ')
+                        return `<svg height="100%" width="100%" style="color: #F1502F" ${sqlServer}`
+                      }
+                      case 'Entity Framework': {
+                        const [_, ef] = svglCsharp.split('svg ')
+                        return `<svg height="100%" width="100%" style="color: #F1502F" ${ef}`
+                      }
                       default: return ''
                     }
                   })()}</div>`
@@ -203,9 +273,6 @@ export class PresentationComponent implements OnInit {
                     td.append(element)
                   }
 
-                  // td.appendChild(span);
-
-                  // td.append(`<ng-icon name="simpleSiemens"></ng-icon>`)
                   tr.appendChild(td);
                   tableBody.appendChild(tr);
                 });
